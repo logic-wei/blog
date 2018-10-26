952dc84303342af09ea17c19b290894828c4faf4
logic-wei <logic_wei@163.com>
2018-10-27 00:57:10 +0800

    优化gitlog.py命令解析器

--- tools/gitblog.py
+++ tools/gitblog.py
@@ -1,7 +1,7 @@
 import argparse
 
 
-def create_category(name):
+def check_create_category(name):
     print("create_category", name)
     pass
 
@@ -16,33 +16,45 @@ def build_summary(path):
     pass
 
 
+def on_sub_create(args):
+    check_create_category(args.category)
+    if args.article:
+        create_article(args.article, args.category)
+
+
+def on_sub_summary(args):
+    build_summary(args.directory)
+
+
+def on_main(args):
+    print("set option -h or --help for help")
+
+
 def main():
-    argparser = argparse.ArgumentParser()
-    argparser.add_argument("command",
-                           choices=("create", "summary"),
-                           nargs="?")
-    argparser.add_argument("-c", "--category",
-                           help="specify a category or create a new category")
-    argparser.add_argument("-a", "--article",
-                           help="start a new article")
-    argparser.add_argument("-d", "--directory",
-                           help="specify a directory to save summary")
-    argparser.add_argument("-v", "--version",
-                           action="version",
-                           version="v1.0")
-    args = argparser.parse_args()
-    if args.command:
-        if args.command == "create":
-            if args.article:
-                create_article(args.article, args.category)
-            else:
-                create_category(args.category)
-        elif args.command == "summary":
-            build_summary(args.directory)
-        else:
-            print("command can't be supported!")
-    else:
-        print("command not found!")
+    # parser setting
+    mainparser = argparse.ArgumentParser()
+    subparsers = mainparser.add_subparsers()
+    # top level parser
+    mainparser.add_argument("-v", "--version",
+                            action="version",
+                            version="v1.0")
+    mainparser.set_defaults(func=on_main)
+    # sub parser for create
+    subparser_create = subparsers.add_parser("create",
+                                             help="create a category or  start a new article")
+    subparser_create.add_argument("-c", "--category",
+                                  help="specify a category or create a new category")
+    subparser_create.add_argument("-a", "--article",
+                                  help="start a new article")
+    subparser_create.set_defaults(func=on_sub_create)
+    # sub parser for summary
+    subparser_summary = subparsers.add_parser("summary", help="build summary")
+    subparser_summary.add_argument("-d", "--directory",
+                                   help="specify a directory to save summary")
+    subparser_summary.set_defaults(func=on_sub_summary)
+    # parse args
+    args = mainparser.parse_args()
+    args.func(args)
 
 
 if __name__ == "__main__":
