dcf4026ffa18ed99283e73b8f54b73009b7bfed3
logic-wei <logic_wei@163.com>
2018-10-27 19:56:24 +0800

    实现create命令

--- tools/gitblog.py
+++ tools/gitblog.py
@@ -1,19 +1,56 @@
 import argparse
+import os
+import sys
+import datetime
+
+# cross platform path
+path_root = os.getcwd()
+path_articles = os.path.join(os.getcwd(), "articles")
+
+
+def err(msg):
+    print("error:"+msg, file=sys.stderr)
+
+
+def info(msg):
+    print("info:"+msg, file=sys.stdout)
 
 
 def check_create_category(name):
-    print("create_category", name)
-    pass
+    if name:
+        files = os.listdir(path_articles)
+        if name in files:
+            info("category "+name+" is already existed.")
+            return
+        else:
+            os.mkdir(os.path.join(path_articles, name))
+            info("directory "+name+" has been created.")
 
 
 def create_article(name, category=None):
-    print("create_article", name, category)
-    pass
+    # where to create article
+    path_category = path_articles
+    if category:
+        path_category = os.path.join(path_articles, category)
+    # check file name conflict
+    files = os.listdir(path_category)
+    if name in files:
+        err("repetitive article:"+name)
+        return
+    # create new article
+    path_article = os.path.join(path_category, name)
+    os.mkdir(path_article)
+    path_main_md = os.path.join(path_article, "main.md")
+    with open(path_main_md, mode="a") as article:
+        article.writelines("# %s\n\n" % name)
+        article.writelines("date:%s\n" % datetime.date.today())
+    info("article %s has been created!" % name)
+    info("path:%s" % path_article)
+    info("date:%s" % datetime.date.today())
 
 
 def build_summary(path):
-    print("build_summary", path)
-    pass
+    info("build_summary", path)
 
 
 def on_sub_create(args):
@@ -27,7 +64,7 @@ def on_sub_summary(args):
 
 
 def on_main(args):
-    print("set option -h or --help for help")
+    info("set option -h or --help for help")
 
 
 def main():
